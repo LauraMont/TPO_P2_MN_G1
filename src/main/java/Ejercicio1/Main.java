37e9fbe3e9b8d18460a038836741b502d27915e5
Agustin <agutorres@frba.utn.edu.ar>
2024-04-30 16:52:56 -0300

    Ejercicio 2

---
+++ src/main/java/Ejercicio1/Main.java
@@ -0,0 +1,37 @@
+package Ejercicio1;
+
+import Ejercicio1.datastructures.QueueOfStacks;
+import Ejercicio1.utils.MatrixOperations;
+
+public class Main {
+    public static void main(String[] args) {
+        // Creación de una matriz 3x3
+        QueueOfStacks matrix = new QueueOfStacks(3);
+        matrix.push(0, 1); matrix.push(0, 2); matrix.push(0, 3);
+        matrix.push(1, 4); matrix.push(1, 5); matrix.push(1, 6);
+        matrix.push(2, 7); matrix.push(2, 8); matrix.push(2, 9);
+
+        // Calculando la traza de la matriz
+        int trace = MatrixOperations.trace(matrix);
+        System.out.println("Traza de la matriz: " + trace);
+
+        // Obteniendo la traspuesta de la matriz
+        QueueOfStacks transposed = MatrixOperations.transpose(matrix);
+        System.out.println("Matriz Traspuesta:");
+        for (int i = 0; i < transposed.getSize(); i++) {
+            System.out.println(transposed.getStacks().get(i));
+        }
+
+        // Sumando dos matrices
+        QueueOfStacks matrix2 = new QueueOfStacks(3);
+        matrix2.push(0, 9); matrix2.push(0, 8); matrix2.push(0, 7);
+        matrix2.push(1, 6); matrix2.push(1, 5); matrix2.push(1, 4);
+        matrix2.push(2, 3); matrix2.push(2, 2); matrix2.push(2, 1);
+
+        QueueOfStacks sumMatrix = MatrixOperations.add(matrix, matrix2);
+        System.out.println("Suma de Matrices:");
+        for (int i = 0; i < sumMatrix.getSize(); i++) {
+            System.out.println(sumMatrix.getStacks().get(i));
+        }
+    }
+}
